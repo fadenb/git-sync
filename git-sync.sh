a3db9eacd6e75ac31076edbabc0c6d2dc6f62d2a
Tristan Helmich (omniIT) <tristan.helmich@omniit.de>
2020-09-25 10:35:32 +0000

    :bomb: naive attempt to push tags

--- git-sync.sh
+++ git-sync.sh
@@ -40,4 +40,5 @@ git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 # Print out all branches
 git --no-pager branch -a -vv
 
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
+#git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
+git push --tags
